527db81212d890c6b1a096d583a56de9eacdd611
zohir hamid <zohirhamid011@gmail.com>
2025-12-18 05:14:25 +0000

    add Bootstrap styling

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "accounts",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -96,3 +99,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
