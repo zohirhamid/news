22d6fc1c7c9bc3b7194c71fc54ab939c88dcb174
zohir hamid <zohirhamid011@gmail.com>
2025-12-22 23:59:41 +0000

    create article

--- articles/urls.py
+++ articles/urls.py
@@ -6,11 +6,13 @@ from .views import (
     ArticleDetailView,
     ArticleUpdateView,
     ArticleDeleteView,
+    ArticleCreateView,
     )
 
 urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
     path("<int:pk>/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/", ArticleDeleteView.as_view(), name="article_delete"),
+    path("new/", ArticleCreateView.as_view(), name="article_new"),
     path("", ArticleListView.as_view(), name="article_list"),
 ]
